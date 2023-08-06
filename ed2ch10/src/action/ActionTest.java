323cf04f9d29497833d8df4bb870d703c8c25f59
erye0 <1299461580@qq.com>
2023-08-06 12:32:33 +0800

    第十章

---
+++ ed2ch10/src/action/ActionTest.java
@@ -0,0 +1,21 @@
+package action;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * @version 1.34 2015-06-12
+ * @author Cay Horstmann
+ */
+public class ActionTest
+{
+   public static void main(String[] args)
+   {
+      EventQueue.invokeLater(() -> {
+         ActionFrame frame = new ActionFrame();
+         frame.setTitle("ActionTest");
+         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         frame.setVisible(true);
+      });
+   }
+}
---
+++ ed2ch10/src/button/ButtonTest.java
@@ -0,0 +1,21 @@
+package button;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * @version 1.35 2018-04-10
+ * @author Cay Horstmann
+ */
+public class ButtonTest
+{
+   public static void main(String[] args)
+   {
+      EventQueue.invokeLater(() -> {
+         ButtonFrame frame = new ButtonFrame();
+         frame.setTitle("ButtonTest");
+         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         frame.setVisible(true);
+      });
+   }
+}
